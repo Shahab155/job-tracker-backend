1a4ba84336e3033257746b743b85b12aa899decc
Shahab Ud din <165199688+Shahab155@users.noreply.github.com>
2026-06-18 15:45:48 +0500

    update

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ app.openapi = custom_openapi
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://job-tracker-frontend-ten-omega.vercel.app","http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
